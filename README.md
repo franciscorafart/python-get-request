5608f8923c741254c6de194a24656661d0ccba11
franciscorafart <francisco.rafart@gmail.com>
2018-01-07 10:13:35 -0500

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python app to make http request to Amazon, get a price from the html and tell the user whether to buy or not depending on price.
+
+What I learned: How to make http requests and look through an html response in Python.
+
+How to run:
+1. Download files
+2. Install required libraries
+3. run $py app.py
+4. Follow me on github
--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-#import library to make http requests
+#import requests library to make http request
 import requests
-#import BeautifulSoup class from bs4 file. Will allow us to parse through html
+#import BeautifulSoup class from bs4 file. Will allow to parse through html
 from bs4 import BeautifulSoup
 
-#get request
+#get request: requesting the price of an amazon article
 request = requests.get("https://www.amazon.com/MXL-770-Cardioid-Condenser-Microphone/dp/B0007NQH98?pf_rd_m=ATVPDKIKX0DER&pf_rd_p=3443758062&pf_rd_r=0e28883a-d1bd-4bcb-bf98-9b22aeae13f2&pd_rd_wg=ItHrL&pf_rd_s=desktop-gateway&pf_rd_t=40701&pd_rd_w=scuL4&pf_rd_i=desktop-gateway&pd_rd_r=0e28883a-d1bd-4bcb-bf98-9b22aeae13f2&ref_=pd_gw_dl_rcs")
 content = request.content
 
 #Using html parser of beautiful soup class
 soup = BeautifulSoup(content, "html.parser")
 
-#parse through the html and look for a span with the established id in JSON format
-#<span id="priceblock_dealprice" class="a-size-medium a-color-price">US$59.95</span>
+#Look for a span with the established id in JSON format
 element = soup.find("span",{"id":"priceblock_dealprice"})
 
 #strip method to remove leading and trailing white space
@@ -19,11 +18,14 @@ string_price = element.text.strip()
 
 #Convert number
 
+#remove $ sign from the price
 #This is copying the string from different indexes. Instead of [start:end] we start from index = 1
 price_without_symbol = string_price[1:]
 
+#convert to float
 price = float(price_without_symbol)
 
+#print out to user
 if (price<60):
     print("Buy! Price is {}".format(price))
 else:
